48db5fca76b1d6aa88de31f541f24a3a4a92fcd7
WestleyK <westleyk@nym.hush.com>
2021-01-27 16:20:11 -0800

    Find the X/Y column automatically based on the csv header

--- pkg/csvParse/csvParse.go
+++ pkg/csvParse/csvParse.go
@@ -17,10 +17,12 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"fmt"
 )
 
 // GetXYDataFromFile will return the X Y data from a csv file path. Returns
-// an error if one occures.
+// an error if one occures. X and Y columns will be determined automatically
+// based on the csv header.
 func GetXYDataFromFile(fileName string) ([]float64, []float64, error) {
 	var xData []float64
 	var yData []float64
@@ -33,25 +35,56 @@ func GetXYDataFromFile(fileName string) ([]float64, []float64, error) {
 
 	// Loop thought the file line-by-line
 	lineCount := 0
+
+	// The default column position
+	xColumn := 1
+	yColumn := 0
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		// Skip the first line
-		// TODO: figure out if the first column is x or y
 		if lineCount == 0 {
+			// This is first line in the file, so we should check if the
+			// first column is X or Y.
+
+			dataArr := strings.Split(scanner.Text(), ",")
+			if len(dataArr) != 2 {
+				return nil, nil, fmt.Errorf("file: %s needs to start with 'x,y'", fileName)
+			}
+
+			if (dataArr[0] != "x" && dataArr[0] != "y") || (dataArr[1] != "x" && dataArr[1] != "y") {
+				return nil, nil, fmt.Errorf("file: %s needs to start with 'x,y' only", fileName)
+			}
+
+			if dataArr[0] == dataArr[1] {
+				return nil, nil, fmt.Errorf("invalid csv header; got: %s; expecting: x,y", scanner.Text())
+			}
+
+			if dataArr[0] == "x" {
+				xColumn = 0
+				yColumn = 1
+			}
+			if dataArr[0] == "y" {
+				yColumn = 0
+				xColumn = 1
+			}
+
 			lineCount++
 			continue
 		}
 		lineValue := scanner.Text()
 
 		dataArr := strings.Split(lineValue, ",")
-		// TODO: make sure theres two slices
+		// Make sure we have the two data values
+		if len(dataArr) != 2 {
+			return nil, nil, fmt.Errorf("missing value in: %s at line: %d", fileName, lineCount)
+		}
 
 		// TODO: for now, y is the first column, and x is the second
-		y, err := strconv.ParseFloat(dataArr[0], 64)
+		y, err := strconv.ParseFloat(dataArr[yColumn], 64)
 		if err != nil {
 			return nil, nil, err
 		}
-		x, err := strconv.ParseFloat(dataArr[1], 64)
+		x, err := strconv.ParseFloat(dataArr[xColumn], 64)
 		if err != nil {
 			return nil, nil, err
 		}
